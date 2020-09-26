c45aa1f2c7d2ac87d78e4e99f7c3052f7ac14163
Iris Gwen J. Caguiat <redseijuro@gmail.com>
2020-09-26 22:06:35 +0800

    [PF-04] Create addToDo() API

--- src/components/to-do/model.ts
+++ src/components/to-do/model.ts
@@ -1,22 +1,10 @@
 import Todo from '../../database/entities/Todo';
-import {getConnection, getManager} from "typeorm";
+import {getManager} from "typeorm";
 
 
 export default class ToDoModel {
-  async addToDo(task: string) {
-     const id: any = await getConnection()
-        .createQueryBuilder()
-        .insert()
-        .into(Todo)
-        .values({
-          task: task
-        }).returning("id")
-        .execute();
-
-     return await getManager()
-          .createQueryBuilder(Todo, "todo")
-          .where("todo.id = :id", { id: id.raw[0].id})
-          .getOne();
-
-  }
+    async addToDo(task: string) {
+        const entityManager = getManager();
+        return await entityManager.save(Todo, {task});
+    }
 }
